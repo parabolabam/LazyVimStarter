4230caf55b77a59094eeed975e2756c2dba943c2
parabolabam <sorokinvladislav1402@gmail.com>
2025-01-03 14:36:16 +0400

    remove unused plugins specs

--- lua/plugins/colorshemes.lua
+++ lua/plugins/colorshemes.lua
@@ -1,12 +1,13 @@
 return {
   { "ellisonleao/gruvbox.nvim" },
+  { "rose-pine/neovim", name = "rose-pine" },
   {
     "rebelot/kanagawa.nvim",
     lazy = false,
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
       -- load the colorscheme here
-      vim.cmd.colorscheme("kanagawa-dragon")
+      vim.cmd.colorscheme("rose-pine")
     end,
   },
 }
