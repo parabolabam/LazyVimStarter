6f8cf5023b67f16ff702d8b650c919338d0fdf08
Vladislav Sorokin <sorokinvladislav1402@gmail.com>
2026-04-28 14:31:24 +0400

    feat: add autosave

--- init.lua
+++ init.lua
@@ -1,2 +1,10 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
+vim.o.autoread = true
+vim.o.updatetime = 250
+
+vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter", "CursorHold", "CursorHoldI" }, {
+  pattern = "*",
+  command = "checktime",
+})
+
 require("config.lazy")
